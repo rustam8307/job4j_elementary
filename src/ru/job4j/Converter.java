4e51667109f3cd4ca36ee54995a5128bb1d1df44
macbookair <rustam8307>
2020-11-25 00:25:26 +0300

    5. Конвертер валюты [#414555]

---
+++ src/ru/job4j/Converter.java
@@ -0,0 +1,22 @@
+package ru.job4j;
+
+public class Converter {
+
+        public static int rubleToEuro(int value) {
+            int rsl = value / 70;
+            return rsl;
+        }
+
+        public static float rubleToDollar(float value) {
+            float rsl = value / 60;
+            return rsl;
+        }
+
+        public static void main(String[] args) {
+            int euro = Converter.rubleToEuro(140);
+            float dollar = Converter.rubleToDollar(140);
+            System.out.println("140 rubles are " + euro + " euro.");
+            System.out.println("140 rubles are " + dollar + " dollar.");
+        }
+    }
+
