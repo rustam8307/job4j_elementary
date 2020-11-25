f708a83bcc919ff19801e34120ac592bc7497e6c
macbookair <rustam8307>
2020-11-25 14:34:29 +0300

    9.1. String [#414566]

---
+++ src/ru/job4j/Condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.Condition;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = " I like Java!";
+        System.out.println(idea);
+        idea = idea + " But i am a newbie.";
+        int year = 2020;
+        System.out.println(idea + year);
+
+    }
+}
