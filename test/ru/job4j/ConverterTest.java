2bc84611581fcc9fe1cf0345947116862f1991c0
macbookair <rustam8307>
2020-11-26 23:13:54 +0300

    1. Модульные тесты. [#414589]

--- test/ru/job4j/ConverterTest.java
+++ test/ru/job4j/ConverterTest.java
@@ -13,11 +13,12 @@ public class ConverterTest {
         Assert.assertEquals(expected, out);
     }
 
+
     @Test
     public void whenConvert180RblThen3Dlr() {
         int in = 180;
         int expected = 3;
-        int out = Converter.rubleToDollar(in);
+        float out = Converter.rubleToDollar(in);
         Assert.assertEquals(expected, out);
     }
 }
