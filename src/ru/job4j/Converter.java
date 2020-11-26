3ae96e5378711fec679ca0313124fcc6302b0074
macbookair <rustam8307>
2020-11-26 18:15:25 +0300

    0. Что такое тестирование [#414588]

--- src/ru/job4j/Converter.java
+++ src/ru/job4j/Converter.java
@@ -2,21 +2,29 @@ package ru.job4j;
 
 public class Converter {
 
-        public static int rubleToEuro(int value) {
-            int rsl = value / 70;
-            return rsl;
-        }
-
-        public static float rubleToDollar(float value) {
-            float rsl = value / 60;
-            return rsl;
-        }
-
-        public static void main(String[] args) {
-            int euro = Converter.rubleToEuro(140);
-            float dollar = Converter.rubleToDollar(140);
-            System.out.println("140 rubles are " + euro + " euro.");
-            System.out.println("140 rubles are " + dollar + " dollar.");
-        }
+    public static int rubleToEuro(int value) {
+        int rsl = value / 70;
+        return rsl;
     }
 
+    public static float rubleToDollar(float value) {
+        float rsl = value / 60;
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        int euro = Converter.rubleToEuro(140);
+        float dollar = Converter.rubleToDollar(140);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("140 rubles are " + dollar + " dollar.");
+        int in = 140;
+        int expected = 2;
+        int out = Converter.rubleToEuro(in);
+        boolean passed = expected == out;
+        System.out.println("140 rubles are 2 euro. Test result : " + passed);
+        float expected2 = 2.3333333f;
+        float out2 = Converter.rubleToDollar(in);
+        boolean passed1 = expected2 == out;
+        System.out.println("140 rubles are 2.3333333 dollar. Test result : " + passed);
+    }
+}
