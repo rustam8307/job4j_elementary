91978eba98d210a2719b718e84944b215a5281d0
macbookair <rustam8307>
2020-11-28 23:52:44 +0300

    1.1. Входные данные в тесте [#414594]

--- src/ru/job4j/Calculate/X2.java
+++ src/ru/job4j/Calculate/X2.java
@@ -9,7 +9,7 @@ public class X2 {
         int a = 10;
         int b = 0;
         int c = 0;
-        int x = 1;
+        int x = 2;
         int rsl = X2.calc(a, b, c, x);
         System.out.println(rsl);
 
