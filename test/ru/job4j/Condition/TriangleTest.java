428514d21ad97dde92dd1179ca6136f083a00bf7
macbookair <rustam8307>
2020-12-03 00:26:22 +0300

    4.3.5. Множественное логическое выражение И [#414519]

--- test/ru/job4j/Condition/TriangleTest.java
+++ test/ru/job4j/Condition/TriangleTest.java
@@ -11,4 +11,9 @@ public class TriangleTest {
         boolean result = Triangle.exist(2.0, 2.0, 2.0);
         assertThat(result, is(true));
     }
+    @Test
+    public void whenExist1() {
+        boolean result = Triangle.exist(1.0, 0.0, 2.0);
+        assertThat(result, is(false));
+    }
 }
