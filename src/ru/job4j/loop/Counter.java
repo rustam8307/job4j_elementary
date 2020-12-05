fa0472d951098279b0096ae84ad9e5e6c6510743
macbookair <rustam8307>
2020-12-06 00:42:04 +0300

    5.0.1. Сумма чисел. [#414618]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -3,31 +3,28 @@ package ru.job4j.loop;
 public class Counter {
     public static int sum(int start, int finish) {
         int sum = 0;
-        for (start = 0; start <= 10; start++) {
-            sum = sum + start;
+        for (int a = start; a <= finish; a++) {
+            sum = sum + a;
         }
         return sum;
     }
 
-    public static int sum1(int start, int finish) {
+    public static int sumByEven(int start, int finish) {
         int sum = 0;
-        for (start = 3; start <= 8; start++) {
-            sum = sum + start;
-        }
-        return sum;
-    }
-
-    public static int sum2(int start, int finish) {
-        int sum = 0;
-        for (start = 1; start <= 1; start++) {
-            sum = sum + start;
+        for (int a = start; a <= finish; a++) {
+            if (a % 2 == 0) {
+                sum = sum + a;
+            }
         }
         return sum;
     }
 
     public static void main(String[] args) {
         System.out.println(sum(0, 10));
-        System.out.println(sum1(3, 8));
-        System.out.println(sum2(1, 1));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
