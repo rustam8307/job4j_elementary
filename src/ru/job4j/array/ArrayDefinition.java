5182d4d500f17635011191fa1326034037d46ce1
macbookair <rustam8307>
2020-12-12 20:49:26 +0300

    6.0.2. Заполнение массива. [#414537]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -10,5 +10,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен surnames: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен prices: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Petr";
+        names[1] = "Alex";
+        names[2] = "Rustam";
+        names[3] = "Vika";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
