ae2102cdb281a36a0211d600a7cf0c0815dad06a
macbookair <rustam8307>
2020-12-12 17:17:17 +0300

    6.0.1.1. Размер массива [#414548]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -1,9 +1,14 @@
 package ru.job4j.array;
 
+import java.sql.SQLOutput;
+
 public class ArrayDefinition {
     public static void main(String[] args) {
         short[] ages = new short[10];
+        System.out.println("Размер массива равен ages: " + ages.length);
         String[] surnames = new String[100500];
+        System.out.println("Размер массива равен surnames: " + surnames.length);
         float[] prices = new float[40];
+        System.out.println("Размер массива равен prices: " + prices.length);
     }
 }
